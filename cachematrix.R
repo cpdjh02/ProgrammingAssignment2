a1f624a8ea2be582988b484814aa4cb33c72614b
David Harris <dharris@avum.com>
2014-06-05 16:33:36 -0500

    Code Update
    Updated the code to work as requested.  Comments still needed.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  ##set: used to set the matrix object. Can be used to change the one
+  ##originally passed in.
+  set <- function(y) {
+    x <<- y
+    ##Setting this null insures that we won't return the inverse of the old matrix
+    cachedInverse <<- NULL 
+  }
+  
+  ##get: used to get the matrix object associated with the cached inverse
+  get <- function() x
+  setInverse <- function(inverse) cachedInverse <<- inverse
+  getInverse <- function() cachedInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +27,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  cachedInverse <- x$getInverse()
+  if(!is.null(cachedInverse)) {
+    message("getting cached data")
+    return(cachedInverse)
+  }
+  data <- x$get()
+  cachedInverse <- solve(data, ...)
+  x$setInverse(cachedInverse)
+  cachedInverse
 }

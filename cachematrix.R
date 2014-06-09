3f5bf6c4e7b686dddddefd4ab4f5369c84026424
David Harris <dharris@avum.com>
2014-06-09 14:41:28 -0500

    Update the comments
    Add comments to the file

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Contains two functions that are used to store the cached value of 
+## the inverse of a matrix.  This could be used to save computation 
+## cost of calculating the inverse of a matrix over and over
+## Example:  
+##  > m <- matrix(c(1,2,3, 11,12,13, 3,5,6), nrow = 3, ncol = 3)
+##  > cachMatrix <- makeCacheMatrix(m)
+##  > invM <- cacheSolve(cachMatrix)  
+## The first time cacheSolve is run on the cachMatrix variable the 
+## inverse of m is calculated and returned.
+##  > invFromCach <- cacheSolve(cachMatrix)
+## any subsequent time it is called the cached value of the inverse 
+## is returned wiht out the need to re-run the inverse calculation
 
-## Write a short comment describing this function
+## makeCacheMatrix: is basically a wrapper around a standard matrix object
+## that provides the ability to cache the invers of the wrapped matrix.
+## It takes an optional signle argument that is the matrix that will have
+## its inverse computed and cashed.
 
 makeCacheMatrix <- function(x = matrix()) {
   cachedInverse <- NULL
@@ -23,7 +36,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve:  This functions takes an object returned by the makeCacheMatrix function
+## which holds a matrix and a cached value of that matrixes inverse.  If the inverse of
+## the matrix has already been calculated it will returned the cached value otherwise it will
+## calculate the inverse, cache it and return it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
